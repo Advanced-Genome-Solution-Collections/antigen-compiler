cbe741388f320cec7b806b3380371e27a5479be3
BuildTools <unconfigured@null.spigotmc.org>
2020-10-19 22:53:12 +0100

    start.c soon to be started

--- start.c
+++ start.c
@@ -1,54 +1,19 @@
+/* ante -- compile from "code" to codons.
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 
 
-void removeSpaces(char *str);
-
 int main(void)
 {
-        char ch[256];
-        char help[256] = "help";
-        int pressed = 0;
-        int var1 = 1;
-        char mode[256] = "normal";
 
-        while (!strcmp(ch, "exit")== 0)
-        {
-                scanf("%s", ch);
-                if (strcmp(ch, help) == 0 || pressed != 0 )
-                {
-
-
-
-
-		}
-
-        }
-        char str[] = " H ellO mY NAD";
-	removeSpaces(str);
-	printf("%d", str);
-	
 	return 0;
 }
 
 
-void removeSpaces(char *str)
-{
-
-	int count = 0;
-	for (int i = 0; str[i]; i++)
-	{	
-		if (str[i] != ' ')
-		{	
-			str[count++] = str[i];
-
-
-		}
-	str[count] = '\0';
-	}
-
-}
 
 
 
